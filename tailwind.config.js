c89786bbc92c17f055068f65651d23dbce8a9f2a
Jonatan Pettersson <jonatan.pettersson@stim.se>
2022-01-29 20:11:59 +0100

    Add IBM Plex Mono

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,16 @@ module.exports = {
     animation: {
       blink: "blink 1s step-end infinite",
     },
+    extend: {},
+    fontFamily: {
+      mono: "'IBM Plex Mono', ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
+    },
     keyframes: {
       blink: {
         "from, to": { "border-color": "transparent" },
-        "50%": { "border-color": "white" },
+        "50%": { "border-color": "rgb(59, 130, 246)" },
       },
     },
-    extend: {},
   },
   plugins: [],
 };
