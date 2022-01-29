36ee3f63c3c9275e9365b3a88e082947526d0998
Jonatan Pettersson <jonatan.pettersson@stim.se>
2022-01-29 19:09:40 +0100

    Add blink animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
+    animation: {
+      blink: "blink 1s step-end infinite",
+    },
+    keyframes: {
+      blink: {
+        "from, to": { "border-color": "transparent" },
+        "50%": { "border-color": "white" },
+      },
+    },
     extend: {},
   },
   plugins: [],
