682d4f6ec97eb3d2d3bf4b3cd71a078acf8a652f
Jonatan Pettersson <jonatan.pettersson@stim.se>
2022-02-19 10:12:10 +0100

    Update conf

--- posthtml.config.js
+++ posthtml.config.js
@@ -6,6 +6,5 @@ module.exports = {
       doctype: "HTML 5",
     },
     "posthtml-include": {},
-    "posthtml-modules": {},
   },
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,9 @@
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    animation: {
-      blink: "blink 1s step-end infinite",
-    },
     extend: {},
-    fontFamily: {
-      mono: "'IBM Plex Mono', ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
-    },
-    keyframes: {
-      blink: {
-        "from, to": { "border-color": "transparent" },
-        "50%": { "border-color": "rgb(59, 130, 246)" },
-      },
-    },
   },
   plugins: [],
 };
